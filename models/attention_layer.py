7d83a110209fb4d407b2a44c32e2492f29851165
Chun-Hao Liu <chun-haoliu@chun-haos-mbp.lan>
2020-09-22 22:19:23 -0700

    add light attention layer

--- models/attention_layer.py
+++ models/attention_layer.py
@@ -4,8 +4,9 @@ This model is to build a attention layer.
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
-__all__ = ['attention_layer']
+__all__ = ['attention_layer', 'attention_layer_light']
 
 class attention_layer(nn.Module):
     def __init__(self, in_dim, texture_dim):
@@ -20,10 +21,10 @@ class attention_layer(nn.Module):
     def forward(self, x, z):
         '''
         input:
-        x: feature maps (batch, C_in, W, H)
-        z: texture images (batch, C_texture, W, H)
+        x: feature maps (batch, C_in, H, W)
+        z: texture images (batch, C_texture, H, W)
         output:
-        out: gamma*attention value + input feature maps (batch, C_in, W, H)
+        out: gamma*attention value + input feature maps (batch, C_in, H, W)
         attention: (batch, N, N), N=W*H
         '''
         batch, C, W ,H = x.size()
@@ -40,19 +41,72 @@ class attention_layer(nn.Module):
 
         return out, attention
 
+class attention_layer_light(nn.Module):
+    def __init__(self, in_dim, texture_dim):
+        super(attention_layer_light, self).__init__()
+        self.conv1 = nn.Conv2d(texture_dim, in_dim, kernel_size=1, stride=1)
+        self.conv2 = nn.Conv2d(in_dim, in_dim, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(in_dim, 1, kernel_size=1, stride=1)
+        self.softmax = nn.Softmax(dim=-1)
+        self.gamma = nn.Parameter(torch.zeros(1))
+        self.in_dim = in_dim
+
+    def forward(self, x, z):
+        '''
+        input:
+        x: feature maps (batch, C_in, Hf, Wf)
+        z: texture images (batch, C_texture, H, W)
+        output:
+        out: gamma*attention value + input feature maps (batch, C_in, Hf, Wf)
+        attention: (batch, Hf, Wf)
+        '''
+        x_origin = x
+        z = self.conv1(z) # (batch, C_in, H, W)
+        z = F.interpolate(z, size=(x.shape[2],x.shape[3]), mode='bilinear') # (batch, C_in, Hf, Wf)
+        x = self.conv2(x) # (batch, C_in, Hf, Wf)
+        combine = self.conv3(torch.tanh(x + z)) # [batch, 1, Hf, Wf]
+        combine_flat = combine.view(combine.size(0), -1) # resize to [batch, Hf*Wf]
+        attention_weights = self.softmax(combine_flat) # [batch, Hf*Wf]
+        attention_weights = attention_weights.view(combine.size()) # [batch, 1, Hf, Wf]
+        glimpse = x_origin * attention_weights.repeat(1, self.in_dim, 1, 1) # [batch, C_in, Hf, Wf]
+        out = self.gamma * glimpse + x_origin # [batch, C_in, Hf, Wf]
+
+        return out, attention_weights
+
 # unit testing
 if __name__ == '__main__':
     # fake input feature map
     batch_size = 2
-    Height = 24
+    Height = 32
     Width = 32
     Channel = 128
+    Height_feature = 32
+    Width_feature = 32
+    Channel_feature = 128
     texture_dim = 1
 
-    features = torch.randn(batch_size,Channel,Height,Width)
+    features = torch.randn(batch_size,Channel_feature,Height_feature,Width_feature)
     texture_image = torch.randn(batch_size, texture_dim, Height, Width)
-    attn_layer = attention_layer(Channel, texture_dim)
+    attn_layer = attention_layer(Channel_feature, texture_dim)
     new_features, att_map = attn_layer(features, texture_image)
     print(new_features)
+    print("shape of new feature map is:", new_features.shape)
+    print("shape of attention map is:", att_map.shape)
+
+    batch_size = 2
+    Height = 640
+    Width = 640
+    Channel = 3
+    Height_feature = 160
+    Width_feature = 160
+    Channel_feature = 128
+    texture_dim = 1
+
+    features = torch.randn(batch_size,Channel_feature,Height_feature,Width_feature)
+    texture_image = torch.randn(batch_size, texture_dim, Height, Width)
+    attn_layer = attention_layer_light(Channel_feature, texture_dim)
+    new_features, att_map = attn_layer(features, texture_image)
+    print(new_features)
+    print(att_map[0][0].sum())
     print("shape of new feature map is:", new_features.shape)
     print("shape of attention map is:", att_map.shape)
