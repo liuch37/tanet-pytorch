4a776a0ad03ac5069ee9d33c2f78bc95db54015f
Chun-Hao Liu <chun-haoliu@chun-haos-mbp.lan>
2020-09-22 22:28:29 -0700

    fix attention

--- models/attention_layer.py
+++ models/attention_layer.py
@@ -54,22 +54,21 @@ class attention_layer_light(nn.Module):
     def forward(self, x, z):
         '''
         input:
-        x: feature maps (batch, C_in, Hf, Wf)
+        x: feature maps (batch, C_in, H, W)
         z: texture images (batch, C_texture, H, W)
         output:
-        out: gamma*attention value + input feature maps (batch, C_in, Hf, Wf)
-        attention: (batch, Hf, Wf)
+        out: gamma*attention value + input feature maps (batch, C_in, H, W)
+        attention: (batch, H, W)
         '''
         x_origin = x
         z = self.conv1(z) # (batch, C_in, H, W)
-        z = F.interpolate(z, size=(x.shape[2],x.shape[3]), mode='bilinear') # (batch, C_in, Hf, Wf)
-        x = self.conv2(x) # (batch, C_in, Hf, Wf)
-        combine = self.conv3(torch.tanh(x + z)) # [batch, 1, Hf, Wf]
-        combine_flat = combine.view(combine.size(0), -1) # resize to [batch, Hf*Wf]
-        attention_weights = self.softmax(combine_flat) # [batch, Hf*Wf]
-        attention_weights = attention_weights.view(combine.size()) # [batch, 1, Hf, Wf]
-        glimpse = x_origin * attention_weights.repeat(1, self.in_dim, 1, 1) # [batch, C_in, Hf, Wf]
-        out = self.gamma * glimpse + x_origin # [batch, C_in, Hf, Wf]
+        x = self.conv2(x) # (batch, C_in, H, W)
+        combine = self.conv3(torch.tanh(x + z)) # [batch, 1, H, W]
+        combine_flat = combine.view(combine.size(0), -1) # resize to [batch, H*W]
+        attention_weights = self.softmax(combine_flat) # [batch, H*W]
+        attention_weights = attention_weights.view(combine.size()) # [batch, 1, H, W]
+        glimpse = x_origin * attention_weights.repeat(1, self.in_dim, 1, 1) # [batch, C_in, H, W]
+        out = self.gamma * glimpse + x_origin # [batch, C_in, H, W]
 
         return out, attention_weights
 
@@ -80,31 +79,25 @@ if __name__ == '__main__':
     Height = 32
     Width = 32
     Channel = 128
-    Height_feature = 32
-    Width_feature = 32
-    Channel_feature = 128
     texture_dim = 1
 
-    features = torch.randn(batch_size,Channel_feature,Height_feature,Width_feature)
+    features = torch.randn(batch_size,Channel,Height,Width)
     texture_image = torch.randn(batch_size, texture_dim, Height, Width)
-    attn_layer = attention_layer(Channel_feature, texture_dim)
+    attn_layer = attention_layer(Channel, texture_dim)
     new_features, att_map = attn_layer(features, texture_image)
     print(new_features)
     print("shape of new feature map is:", new_features.shape)
     print("shape of attention map is:", att_map.shape)
 
-    batch_size = 2
-    Height = 640
-    Width = 640
-    Channel = 3
-    Height_feature = 160
-    Width_feature = 160
-    Channel_feature = 128
+    batch_size = 16
+    Height = 160
+    Width = 160
+    Channel = 512
     texture_dim = 1
 
-    features = torch.randn(batch_size,Channel_feature,Height_feature,Width_feature)
+    features = torch.randn(batch_size,Channel,Height,Width)
     texture_image = torch.randn(batch_size, texture_dim, Height, Width)
-    attn_layer = attention_layer_light(Channel_feature, texture_dim)
+    attn_layer = attention_layer_light(Channel, texture_dim)
     new_features, att_map = attn_layer(features, texture_image)
     print(new_features)
     print(att_map[0][0].sum())
