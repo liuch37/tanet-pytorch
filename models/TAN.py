51f6d688723f7ab0570bbf76fb5ff324cfd0c662
Chun-Hao Liu <chun-hao.liu@ericsson.com>
2020-09-24 16:32:30 -0700

    modify TAN

--- models/TAN.py
+++ models/TAN.py
@@ -7,11 +7,31 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.parameter import Parameter
 from torch.nn import ParameterList
+import math
 from .gradient_layer import cgradient
 from .gabor_layer import gabor_layer
-from .attention_layer import attention_layer
+from .attention_layer import attention_layer, attention_layer_light
 
-__all__ = ['TANet']
+__all__ = ['Conv_BN_ReLU','TANet']
+
+class Conv_BN_ReLU(nn.Module):
+    def __init__(self, in_planes, out_planes, kernel_size=1, stride=1, padding=0):
+        super(Conv_BN_ReLU, self).__init__()
+        self.conv = nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=padding,
+                              bias=False)
+        self.bn = nn.BatchNorm2d(out_planes)
+        self.relu = nn.ReLU(inplace=True)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def forward(self, x):
+        return self.relu(self.bn(self.conv(x)))
 
 class TANet(nn.Module):
     def __init__(self, f_in, C_in, kernel_sizes=[3], theta_size=4):
@@ -25,6 +45,9 @@ class TANet(nn.Module):
         self.gradient_layer = cgradient(C_in, kernel_size=3)
         self.gabor_layers = nn.Sequential(*[gabor_layer(theta_size, kernel_size=k) for k in kernel_sizes])
         #self.attention_layer = attention_layer(f_in, 1)
+        self.attention_layer_light = attention_layer_light(f_in)
+        self.reduce_layer1 = Conv_BN_ReLU(1+theta_size, 256, kernel_size=1, stride=2, padding=0)
+        self.reduce_layer2 = Conv_BN_ReLU(256, 512, kernel_size=1, stride=2, padding=0)
         self.alphas = ParameterList([Parameter((torch.Tensor([1.0/len(kernel_sizes)]))) for i in range(len(kernel_sizes))])
         self.beta = Parameter(torch.Tensor([1.0]))
         self.kernel_sizes = kernel_sizes
@@ -40,15 +63,18 @@ class TANet(nn.Module):
         # fusion
         img_fusion = torch.cat((img_gabor[0], self.beta*img_grad), dim=1)
 
-        # resize
-        feature_map = F.interpolate(img_fusion, size=(feature_map.shape[2],feature_map.shape[3]), mode='bilinear')
+        # downsample
+        img_fusion = self.reduce_layer1(img_fusion)
+        img_fusion = self.reduce_layer2(img_fusion)
+        
+        # add
+        img_fusion = feature_map + img_fusion
 
-        '''
         # attention
-        feature_map, att_map = self.attention_layer(feature_map, img_fusion)
-        '''
+        #feature_map, att_map = self.attention_layer(feature_map, img_fusion)
+        feature_map, att_map = self.attention_layer_light(img_fusion)
 
-        return feature_map
+        return feature_map, att_map
 
 # unit testing
 if __name__ == '__main__':
@@ -56,7 +82,7 @@ if __name__ == '__main__':
     batch_size = 2
     Height_feature = 160
     Width_feature = 160
-    Channel_feature = 128
+    Channel_feature = 512
     Height_input = 640
     Width_input = 640
     Channel_input = 3
@@ -65,7 +91,7 @@ if __name__ == '__main__':
     input_images = torch.randn(batch_size, Channel_input, Height_input, Width_input)
 
     model = TANet(f_in=Channel_feature, C_in=Channel_input, kernel_sizes=[3, 5], theta_size=4)
-    feature_map = model(features, input_images)
+    feature_map, att_map = model(features, input_images)
 
     print(feature_map)
     for name, parameter in model.named_parameters():
@@ -77,3 +103,4 @@ if __name__ == '__main__':
     print("Input feature map size is:", features.shape)
     print("Input image size is:", input_images.shape)
     print("Outout feature map size is:", feature_map.shape)
+    print("Outout attention map size is:", att_map.shape)
