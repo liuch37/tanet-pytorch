4554d3978cfb7adcdb1e60214c905c5797bbd44c
Chun-Hao Liu <chun-hao.liu@ericsson.com>
2020-09-24 15:52:40 -0700

    modify attention layer light

--- models/attention_layer.py
+++ models/attention_layer.py
@@ -42,28 +42,25 @@ class attention_layer(nn.Module):
         return out, attention
 
 class attention_layer_light(nn.Module):
-    def __init__(self, in_dim, texture_dim):
+    def __init__(self, in_dim):
         super(attention_layer_light, self).__init__()
-        self.conv1 = nn.Conv2d(texture_dim, in_dim, kernel_size=1, stride=1)
-        self.conv2 = nn.Conv2d(in_dim, in_dim, kernel_size=3, stride=1, padding=1)
-        self.conv3 = nn.Conv2d(in_dim, 1, kernel_size=1, stride=1)
+        self.conv1 = nn.Conv2d(in_dim, in_dim, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(in_dim, 1, kernel_size=1, stride=1)
         self.softmax = nn.Softmax(dim=-1)
         self.gamma = nn.Parameter(torch.zeros(1))
         self.in_dim = in_dim
 
-    def forward(self, x, z):
+    def forward(self, x):
         '''
         input:
         x: feature maps (batch, C_in, H, W)
-        z: texture images (batch, C_texture, H, W)
         output:
         out: gamma*attention value + input feature maps (batch, C_in, H, W)
         attention: (batch, H, W)
         '''
         x_origin = x
-        z = self.conv1(z) # (batch, C_in, H, W)
-        x = self.conv2(x) # (batch, C_in, H, W)
-        combine = self.conv3(torch.tanh(x + z)) # [batch, 1, H, W]
+        x = self.conv1(x) # (batch, C_in, H, W)
+        combine = self.conv2(torch.tanh(x)) # [batch, 1, H, W]
         combine_flat = combine.view(combine.size(0), -1) # resize to [batch, H*W]
         attention_weights = self.softmax(combine_flat) # [batch, H*W]
         attention_weights = attention_weights.view(combine.size()) # [batch, 1, H, W]
@@ -93,12 +90,10 @@ if __name__ == '__main__':
     Height = 160
     Width = 160
     Channel = 512
-    texture_dim = 1
 
     features = torch.randn(batch_size,Channel,Height,Width)
-    texture_image = torch.randn(batch_size, texture_dim, Height, Width)
-    attn_layer = attention_layer_light(Channel, texture_dim)
-    new_features, att_map = attn_layer(features, texture_image)
+    attn_layer = attention_layer_light(Channel)
+    new_features, att_map = attn_layer(features)
     print(new_features)
     print(att_map[0][0].sum())
     print("shape of new feature map is:", new_features.shape)
